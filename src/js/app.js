c87ee8ab4be7af64825b3525cb0523b08180de6d
Jonathan OBrien <jonathan.r.obrien@gmail.com>
2020-03-05 10:21:40 -0500

    Move logic into app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,17 @@
 (function(window){
   window.smartApp = {};
 
+  smartApp.authorizeApplication = function() {
+    FHIR.oauth2.authorize({
+        'clientId': 'faf44bec-04fb-47aa-8d31-8dfe3eff1720',
+        'scope': 'patient/*.read launch profile openid online_access',
+        'redirectUri': 'https://nanofied.github.io/smart-application-template/app.html'
+      })
+      .catch(function(reason) {
+        console.error(reason);
+      });
+  };
+
   smartApp.extractData = function(client) {
     var patient = client.request('Patient');
     console.log(patient);
