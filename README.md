e03721fbffb37b45c8e817b11ff3db9bcecf1ae3
Jonathan OBrien <jonathan.r.obrien@gmail.com>
2020-03-05 08:44:30 -0500

    Initial commit
    Push base code to repository.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bilirubin Risk SMART Application
+
+## Setup
+
+The Bilirubin Risk SMART Application is a static web application. Copy it to a web server, modify the configuration file, and show off your new SMART application.
---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+(function(window){
+  window.smartApp = {};
+
+  smartApp.extractData = function(client) {
+    var patient = client.request('Patient');
+  };
+
+  smartApp.renderHTML = function() {
+    // Add logic for rendering extracted data in HTML
+  };
+
+})(window);
